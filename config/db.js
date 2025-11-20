b0e4618376fecb454d681c29f29ddc267630aa94
urboy-code <42699288+urboy-code@users.noreply.github.com>
2025-11-20 08:22:17 +0700

    fix: Update db config for production

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   connectionString: process.env.DATABASSE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
+  pool,
 };
